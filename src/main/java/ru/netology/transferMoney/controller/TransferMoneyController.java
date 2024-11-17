cee63e9d12ff581ba16a1253a1a5d7c6ed7ac6a1
Nikitastolyarow <nikita.stolyarow2002@yandex.ru>
2024-11-17 22:43:51 +0300

    Создание TransferMoneyController

---
+++ src/main/java/ru/netology/transferMoney/controller/TransferMoneyController.java
@@ -0,0 +1,23 @@
+package ru.netology.transferMoney.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransferMoneyController {
+    private final TransferMoneyController transferMoneyController;
+
+    @Autowired
+    public TransferMoneyController(TransferMoneyController transferMoneyController) {
+        this.transferMoneyController = transferMoneyController;
+    }
+
+    //Обработка запроса на перевод денег
+    @PostMapping("/transfer")
+
+    // Обработка подтверждения перевода
+    @PostMapping("/confirmOperation")
+    
+}
